d79ff3f0e451fd1d121d2d5ce18a197dbc20ab01
Rafael Souza <snrafa.souza@gmail.com>
2022-07-08 15:38:08 -0300

    Adjustments in ResourceServerConfig

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -37,7 +37,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     }
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception { 
         
         http.authorizeRequests()
         .antMatchers(PUBLIC).permitAll()
