31a86cf9e1449cb4b5ecd04f67dd26090cb05c98
Rafael Souza <snrafa.souza@gmail.com>
2022-07-08 16:19:55 -0300

    dv config server

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -24,9 +24,6 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Value("${test.config}")
-    private String testConfig;
-
     @Autowired
     private Environment env;
     
@@ -35,7 +32,7 @@ public class WorkerResource {
 
     @GetMapping("/configs")
     public ResponseEntity<Void> getConfigs() {
-        logger.info("CONFIG = " + testConfig);
+        //logger.info("CONFIG = " + testConfig);
         return ResponseEntity.noContent().build();
     }
  
@@ -48,14 +45,14 @@ public class WorkerResource {
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-        
+        /*
         try {
 			Thread.sleep(3000L);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+         */
         
-
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
         Worker obj = workerRepository.findById(id).get();
